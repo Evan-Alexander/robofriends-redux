536dbf7b7b1242a31c7d370303ed3e61cd76f767
jb <jasontbrown99@gmail.com>
2018-04-27 15:23:55 -0700

    initial commit; searchfield wired to redux

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Jaime Brown',
+    username: 'Scooter-magoo',
+    email: 'junk@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'John Smith',
+    username: 'fraggle-monster',
+    email: 'junkmail@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Jason Jackson',
+    username: 'charlie',
+    email: 'junkart@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Gary Lockheard',
+    username: 'limpbisquit',
+    email: 'junktown@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'David Funpants',
+    username: 'goof',
+    email: 'junkathon@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Margie Maggles',
+    username: 'moviephanatic',
+    email: 'junkbots@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Sioux Soothnes',
+    username: 'zeemoney',
+    email: 'zjunk@gmail.com'
+  },
+]
